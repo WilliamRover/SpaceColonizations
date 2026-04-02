d8df56655fe976d13f4ef473ea4f368910b1b09f
William Rover <roverwilliam4567@gmail.com>
2026-04-02 14:48:39 +0300

    Added menu page

---
+++ app/src/main/java/com/example/spacecolonizations/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.example.spacecolonizations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        setupButton();
+    }
+
+    private void setupButton() {
+        Button btnContinue = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnNewGame = findViewById(R.id.btnNewGame);
+        btnNewGame.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
