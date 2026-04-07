b51449af4b6e474635be692a0cabf43151b213fe
arkriderx <arkriderx@gmail.com>
2026-04-07 11:06:23 +0300

    money ship

--- app/src/main/java/com/example/spacecolonizations/model/ship/FriendlyShip.java
+++ app/src/main/java/com/example/spacecolonizations/model/ship/FriendlyShip.java
@@ -12,12 +12,27 @@ public class FriendlyShip extends Ship{
     private int innitEnergy;
     private List<Crew> crews;
     private int totalCrew;
+    private int money;
 
     private TextView friendlyExplode;
 
     public FriendlyShip(int innitHullStrength, int innitEnergy) {
         super(innitHullStrength);
+        this.money = 0;
     }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void addMoney(int n){
+        money = money+n;
+    }
+
+    public void reduceMoney(int n){
+        money = money-n;
+    }
+
     public void recruitCrew(Crew c) {
         crews.add(c);
         totalCrew++;
