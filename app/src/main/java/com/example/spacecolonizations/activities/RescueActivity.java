28103c1f1b2521b2a6fea98099624e0e492de11d
arkriderx <arkriderx@gmail.com>
2026-04-12 17:43:02 +0300

    Add setObstaclesType method to PassObstacle class

--- app/src/main/java/com/example/spacecolonizations/activities/RescueActivity.java
+++ app/src/main/java/com/example/spacecolonizations/activities/RescueActivity.java
@@ -3,13 +3,17 @@ package com.example.spacecolonizations.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.spacecolonizations.NameGen;
 import com.example.spacecolonizations.R;
+import com.example.spacecolonizations.model.crewmate.Crew;
 import com.example.spacecolonizations.model.crewmate.CrewManager;
 import com.example.spacecolonizations.model.mission.Rescue;
+
+import java.util.ArrayList;
 
 public class RescueActivity extends AppCompatActivity {
     @Override
@@ -23,6 +27,17 @@ public class RescueActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        TextView textView5 = findViewById(R.id.textView5);
+
+        ArrayList<String> crewName = new ArrayList<>();
+
         Rescue rescue = new Rescue(NameGen.nGen((int) ((Math.random()*5) + 3)));
+        for (int i = 0; i<2; i++){
+            Crew c = CrewManager.getCrew().get((int) (Math.random()*CrewManager.getCrew().size()));
+            rescue.addCrew(c);
+            crewName.add(c.getName());
+        }
+        String outRescueTxt = crewName.get(0) + " and " + crewName.get(1) + " have been sent to aid ally ships";
+        textView5.setText(outRescueTxt);
     }
 }
