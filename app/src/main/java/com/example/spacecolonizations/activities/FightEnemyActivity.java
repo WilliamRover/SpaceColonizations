46a5f74cb755fc24f8066ef12ff15b65e69167d1
William Rover <roverwilliam4567@gmail.com>
2026-04-12 16:27:24 +0300

    Minor UI thingy

--- app/src/main/java/com/example/spacecolonizations/activities/FightEnemyActivity.java
+++ app/src/main/java/com/example/spacecolonizations/activities/FightEnemyActivity.java
@@ -14,6 +14,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.spacecolonizations.R;
@@ -50,6 +51,7 @@ public class FightEnemyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         EdgeToEdge.enable(this);
         setContentView(R.layout.fight_enemy);
 
@@ -64,7 +66,6 @@ public class FightEnemyActivity extends AppCompatActivity {
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
